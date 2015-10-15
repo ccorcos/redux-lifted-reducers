7713ca0cdaeb6dc4f15ec242ac3e97061851e79e
Chet Corcos <ccorcos@gmail.com>
2015-10-14 21:58:13 -0700

    lifted actions and reducers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { combineReducers, createStore, bindActionCreators} from 'redux';
 
+// -----------------------------------------------------------------------------
+
 // counter action creators
 const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
 const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
@@ -20,51 +22,89 @@ function decrement() {
 // counter reducer
 function count(state = 0, action) {
   switch (action.type) {
-  case INCREMENT_COUNTER:
-    return state + 1;
-  case DECREMENT_COUNTER:
-    return state - 1;
-  default:
-    return state;
+    case INCREMENT_COUNTER:
+      return state + 1;
+    case DECREMENT_COUNTER:
+      return state - 1;
+    default:
+      return state;
   }
 }
 
 // counter component
 class Counter extends Component {
-  render() {
-    const { increment, decrement, count } = this.props;
-    return (
-      <p>
-        Clicked: {count} times
-        {' '}
-        <button onClick={increment}>+</button>
-        {' '}
-        <button onClick={decrement}>-</button>
-      </p>
-    );
-  }
-}
-
-
-const reducer = combineReducers({
-  count
-});
-
-const store = createStore(reducer)
-
-
-class App extends Component {
   componentWillMount() {
+    const {increment, decrement} = this.props;
     this.actions = bindActionCreators(
       {increment, decrement},
       this.props.dispatch
     )
   }
   render() {
+    const { count } = this.props;
     return (
-      <Counter count={this.props.state.count}
-               increment={this.actions.increment}
-               decrement={this.actions.decrement}/>
+      <p>
+        Clicked: {count} times
+        {' '}
+        <button onClick={this.actions.increment}>+</button>
+        {' '}
+        <button onClick={this.actions.decrement}>-</button>
+      </p>
+    );
+  }
+}
+
+// -----------------------------------------------------------------------------
+
+// nest the action behind another action
+const liftActionCreator = liftingAction => actionCreator => action => Object.assign({}, liftingAction, { nextAction: actionCreator(action) })
+// a lifting reducer will consume the first action and apply the next reducer to the state
+const liftReducer = liftingReducer => reducer => (state, action) => liftingReducer(state, action)((subState) => reducer(subState, action.nextAction))
+
+// -----------------------------------------------------------------------------
+
+// list actions
+const LIST_INDEX = 'LIST_INDEX'
+function actOnIndex(i) {
+  return {
+    type: LIST_INDEX,
+    index: i
+  }
+}
+
+// list high-order reducer
+const list = (state=[], action) => (reduce) => {
+  switch (action.type) {
+    case LIST_INDEX:
+      let nextState = state.slice(0)
+      nextState[action.index] = reduce(nextState[action.index])
+      return nextState
+    default:
+      return state;
+  }
+}
+
+const reducer = combineReducers({
+  counts: liftReducer(list)(count)
+});
+
+const store = createStore(reducer)
+
+class App extends Component {
+  render() {
+    const counters = [0,1,2,3,4].map((i) => {
+      return (
+        <Counter count={this.props.state.counts[i]}
+                 increment={liftActionCreator(actOnIndex(i))(increment)}
+                 decrement={liftActionCreator(actOnIndex(i))(decrement)}
+                 dispatch={this.props.dispatch}
+                 key={i}/>
+      )
+    })
+    return (
+      <div>
+        {counters}
+      </div>
     );
   }
 }
